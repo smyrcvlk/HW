8667c99f477c252e91c1da9fd61deda9b1f992ca
TechnoStudy <info@techno.study>
2019-11-21 19:34:59 -0500

    homework refactored

---
+++ src/day35/ConstructorsAndAccessModifiers.java
@@ -0,0 +1,7 @@
+package day35;
+
+public class ConstructorsAndAccessModifiers {
+    // Constructors and Access Modifiers
+
+
+}
---
+++ src/day35/homework/constructor/HW.java
@@ -0,0 +1,10 @@
+package day35.homework.constructor;
+
+//create a class
+// add at least 3 attributes
+// add at least 3 constructors
+// add method that return all attributes as one string
+// in HW class(in different) create at least 4 object on that class
+// call method to get attributes and print it
+public class HW {
+}

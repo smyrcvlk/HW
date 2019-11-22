5058e94315667e75ec21ec6b3056e6a9a7babd7d
TechnoStudy <info@techno.study>
2019-11-21 21:30:31 -0500

    36 added

---
+++ src/day36/homework/HW.java
@@ -0,0 +1,8 @@
+package day36.homework;
+
+public class HW {
+    //TODO TRY CATCH HOMEWORK
+    // 1. use string "HelloWorld" and get char at index 100
+    // 2. create array of size 10, create a loop from 0 to 100, and try to print every element in array
+
+}

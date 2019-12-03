6be174d523499ca4a7e1d8ecb796072d8534b1bd
smyrcvlk <smyrcvlk@gmail.com>
2019-12-02 19:59:59 -0500

    hw

---
+++ src/day43/HW.java
@@ -0,0 +1,13 @@
+package day43;
+
+public class HW {
+    // Let's write a Online-Shop that sells books: ChildrenBook & Comics
+    // use inheritance, polymorphism and encapsulation while doing this
+
+    // Note: this HW is open-ended, so there a lot of way to solve it
+    // task about salary is similar to this
+
+    // Hint: you can use Book as parent class and define price() method there
+
+
+}
